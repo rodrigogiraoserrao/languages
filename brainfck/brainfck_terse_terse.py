966efa6698373d0d39522e0d9c49ee3a0f4c0a7e
Rodrigo Girão Serrão <rodrigogiraoserrao@gmail.com>
2020-11-12 18:03:30 +0000

    Simplify getting input a bit.

--- brainfck/brainfck_terse_terse.py
+++ brainfck/brainfck_terse_terse.py
@@ -4,7 +4,7 @@ def I(b,i="",t=[0],p=0,o=""):
   c,*b=b;c=C.find(c) # get next op
   if c in[0,1]:t[p]+=1-2*c;t[p]%=M # increase memory cell and wrap at 256
   if c in[2,3]:p+=5-2*c;t=[0]*(p<0)+t+[0]*(p==len(t));p=max(p,0) # move pointer and adjust tape
-  if c==4:t[p]=ord(i[0])%M if i else 0; i=i[1:]if i else "" # read one char as numeric input
+  if c==4:i=i if i else chr(0);i_,*i=i;t[p]=ord(i_)%M # read one char as numeric input
   if c==5:o+=chr(t[p]) # print one char as output
   if c==6:
    d=1;j=[d:=d+(x=="[")-(x=="]")for x in b].index(0);b,b_=b[j+1:],b[:j]
